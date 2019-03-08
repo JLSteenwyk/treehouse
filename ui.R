314abba948bdeb91230cd9d6ba7ba108d065ad81
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2019-03-08 16:25:22 -0600

    added download newick function

--- ui.R
+++ ui.R
@@ -12,16 +12,19 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   # generate a page title
-  titlePanel("PhyloViewer"),
+  titlePanel("Aspergillus and Penicillium subtrees"),
 
   # input file
   fileInput("file", label = h3("File input")),
+  p("Plot subtrees of select species in the phylogeny of Aspergillus and Penicillium species"),
   hr(),
   
   # Create a spot for the barplot
   mainPanel(
-    plotOutput("phyloPlot", width = "100%"),
     downloadButton(outputId = "TreePlot", label = "Download Plot"),
-    downloadButton(outputId = "Newick", label = "Download Newick File")
+    downloadButton(outputId = "Newick", label = "Download Newick File"),
+    plotOutput("phyloPlot", width = "100%"),
+    
+    
   )
 ))
