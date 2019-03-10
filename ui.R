e5c7968173ccad7de971d58ac8face26680b6d1f
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2019-03-09 19:57:50 -0600

    added pdf logo

--- ui.R
+++ ui.R
@@ -38,7 +38,7 @@ shinyUI(fluidPage(
     ),
     
     mainPanel(
-    plotOutput("phyloPlot", width = "100%", height = "500px"),
+    plotOutput("phyloPlot", width = "100%", height = "100%"),
     downloadButton(outputId = "TreePlot", label = "Download Plot"),
     downloadButton(outputId = "Newick", label = "Download Newick File"),
     hr(),
