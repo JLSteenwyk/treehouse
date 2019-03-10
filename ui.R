0c469f0d188699cc4f97697a04dc7f5b6d593d73
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2019-03-09 19:18:32 -0600

    added pdf logo

--- ui.R
+++ ui.R
@@ -12,7 +12,7 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   # display logo
-  titlePanel(title=div(img(src="./logo/treehouse_logo.png",""))),
+  titlePanel(title=div(img(src="treehouse_logo.png",""))),
   h4("get subtrees from the phylogenies in our treehouse"), 
   hr(),
   # Create a spot for the choices
