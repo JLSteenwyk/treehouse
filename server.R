c5fe89f44f01007bdd75c694b4a78e01a0c22802
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2019-03-18 09:21:14 -0500

    added C. neoformans phylogeny of 387 strains

--- server.R
+++ server.R
@@ -9,7 +9,7 @@
 
 
 ## Load necessary packages
-list.of.packages <- c("shiny", "phytools")
+list.of.packages <- c("phytools")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 #library(shiny)
