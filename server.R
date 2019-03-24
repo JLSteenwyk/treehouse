b65461d9fe5d5f2b162419ad44ad6415b96c43a6
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2019-03-24 08:49:49 -0500

    testing fungi subpage

--- server.R
+++ server.R
@@ -19,7 +19,7 @@ shinyServer(function(input, output, session) {
   
   ### FUNGI
   ## read in file reactively
-  FUNGIdata <- eventReactive(input$FUNGIgo, { 
+  data <- eventReactive(input$FUNGIgo, { 
     req(input$file)
     inFile <- input$file
     taxa.list<-read.table(inFile$datapath)
@@ -27,54 +27,54 @@ shinyServer(function(input, output, session) {
     })
 
   ## read in tree based on selected tree
-  FUNGItree <- eventReactive(input$FUNGIgo, {
+  tree <- eventReactive(input$FUNGIgo, {
     if (input$FUNGIphyloSelect == "Aspergillaceae, 81 taxa - Steenwyk et al. 2018") {
-      FUNGItree <- read.tree("./Data/Aspergillaceae_fig1_Steenwyk_etal_2018.tre")
+      tree <- read.tree("./Data/Aspergillaceae_fig1_Steenwyk_etal_2018.tre")
     } else if (input$FUNGIphyloSelect == "Saccharomycotina, 86 taxa - Shen et al. 2016") {
-      FUNGItree <- read.tree("./Data/Saccharomycotina_fig3_Shen_etal_2016.tre")
+      tree <- read.tree("./Data/Saccharomycotina_fig3_Shen_etal_2016.tre")
     } else if (input$FUNGIphyloSelect == "Saccharomycotina, 332 taxa - Shen et al. 2018") {
-      FUNGItree <- read.tree("./Data/Saccharomycotina_fig2_Shen_etal_2018.tre")
+      tree <- read.tree("./Data/Saccharomycotina_fig2_Shen_etal_2018.tre")
     } else if (input$FUNGIphyloSelect == "Saccharomyces cerevisiae, 1,011 strains - Peter et al. 2018") {
-      FUNGItree <- read.tree("./Data/Saccharomyces_cerevisiae_fig1_Peter_etal_2018.tre") 
+      tree <- read.tree("./Data/Saccharomyces_cerevisiae_fig1_Peter_etal_2018.tre") 
     } else if (input$FUNGIphyloSelect == "Cryptococcus neoformans, 387 strains - Desjardins et al. 2017") {
-      FUNGItree <- read.tree("./Data/Cryptococcus_neoformans_fig1_Desjardins_etal_2017.tre")  
+      tree <- read.tree("./Data/Cryptococcus_neoformans_fig1_Desjardins_etal_2017.tre")  
     } else if (input$FUNGIphyloSelect == "Fungi, 214 species - James et al. 2006") {
-      FUNGItree <- read.nexus("./Data/Fungi_fig1_James_etal_2006.nex.tre")  
+      tree <- read.nexus("./Data/Fungi_fig1_James_etal_2006.nex.tre")  
     } else if (input$FUNGIphyloSelect == "Fungi, 5,284 species - Varga et al. 2019") {
-      FUNGItree <- read.nexus("./Data/Fungi_fig1_Varga_etal_2019.nex.tre")  
+      tree <- read.nexus("./Data/Fungi_fig1_Varga_etal_2019.nex.tre")  
     } else if (input$FUNGIphyloSelect == "Metazoans, 36 taxa - Borowiec et al. 2015") {
-      FUNGItree <- read.tree("./Data/Metazoans_fig3_Borowiec_etal_2015.tre")  
+      tree <- read.tree("./Data/Metazoans_fig3_Borowiec_etal_2015.tre")  
     } else if (input$FUNGIphyloSelect == "Tree of life, 3,083 taxa - Hug et al. 2016") {
-      FUNGItree <- read.tree("./Data/Tree_of_Life_fig1_Hug_etal_2016.tre") 
+      tree <- read.tree("./Data/Tree_of_Life_fig1_Hug_etal_2016.tre") 
     } else 
-      FUNGItree <- read.tree("./Data/Aspergillaceae_fig1_Steenwyk_etal_2018.tre")
+      tree <- read.tree("./Data/Aspergillaceae_fig1_Steenwyk_etal_2018.tre")
   })
 
   ## select outgroup labels based on selected tree
-  FUNGIoutgroup.labels <- eventReactive(input$FUNGIgo, {input$FUNGIphyloSelect
+  outgroup.labels <- eventReactive(input$FUNGIgo, {input$FUNGIphyloSelect
     if (input$FUNGIphyloSelect == "Aspergillaceae, 81 taxa - Steenwyk et al. 2018") {
-      FUNGIoutgroup.labels<-c("Neurospora_crassa","Microsporum_canis","Uncinocarpus_reesii","Trichophyton_rubrum","Basipetospora_chlamydospora","Coccidioides_posadasii","Paracoccidioides_brasiliensis","Trichoderma_reesei","Coccidioides_immitis","Histoplasma_capsulatum","Talaromyces_occitanis","Talaromyces_marneffei")
+      outgroup.labels<-c("Neurospora_crassa","Microsporum_canis","Uncinocarpus_reesii","Trichophyton_rubrum","Basipetospora_chlamydospora","Coccidioides_posadasii","Paracoccidioides_brasiliensis","Trichoderma_reesei","Coccidioides_immitis","Histoplasma_capsulatum","Talaromyces_occitanis","Talaromyces_marneffei")
       }
       else if (input$FUNGIphyloSelect == "Saccharomycotina, 86 taxa - Shen et al. 2016") {
-      FUNGIoutgroup.labels<-c("Schizosaccharomyces_pombe","Arthrobotrys_oligospora","Neurospora_crassa","Fusarium_graminearum","Geotrichum_candidum_3C", "Botrytis_cinerea", "Sclerotinia_sclerotiorum", "Stagonospora_nodorum", "Aspergillus_nidulans","Xylona_heveae")
+      outgroup.labels<-c("Schizosaccharomyces_pombe","Arthrobotrys_oligospora","Neurospora_crassa","Fusarium_graminearum","Geotrichum_candidum_3C", "Botrytis_cinerea", "Sclerotinia_sclerotiorum", "Stagonospora_nodorum", "Aspergillus_nidulans","Xylona_heveae")
       }
       else if (input$FUNGIphyloSelect == "Saccharomycotina, 332 taxa - Shen et al. 2018") {
-      FUNGIoutgroup.labels<-c("Saitoella_complicata","Schizosaccharomyces_pombe","Arthrobotrys_oligospora","Fusarium_graminearum","Neurospora_crassa", "Sclerotinia_sclerotiorum", "Botrytis_cinerea", "Stagonospora_nodorum", "Aspergillus_nidulans","Xylona_heveae", "Coccidioides_immitis")
+      outgroup.labels<-c("Saitoella_complicata","Schizosaccharomyces_pombe","Arthrobotrys_oligospora","Fusarium_graminearum","Neurospora_crassa", "Sclerotinia_sclerotiorum", "Botrytis_cinerea", "Stagonospora_nodorum", "Aspergillus_nidulans","Xylona_heveae", "Coccidioides_immitis")
       }
       else if (input$FUNGIphyloSelect == "Saccharomyces cerevisiae, 1,011 strains - Peter et al. 2018") {
-      FUNGIoutgroup.labels<-c("AMH","CEG","CEI")
+      outgroup.labels<-c("AMH","CEG","CEI")
       }
       else if (input$FUNGIphyloSelect == "Cryptococcus neoformans, 387 strains - Desjardins et al. 2017") {
-      FUNGIoutgroup.labels<-c("8-1","AD3-11a","AD3-9a","AD5-67a","C12","C2","C45","LP-RSA1684","MW-RSA1327","MW-RSA3956","MW-RSA4119","MW_RSA852","PMHc1023.ENR","T4","Ug2462","WM626")
+      outgroup.labels<-c("8-1","AD3-11a","AD3-9a","AD5-67a","C12","C2","C45","LP-RSA1684","MW-RSA1327","MW-RSA3956","MW-RSA4119","MW_RSA852","PMHc1023.ENR","T4","Ug2462","WM626")
       }
       else if (input$FUNGIphyloSelect == "Fungi, 214 species - James et al. 2006") {
-      FUNGIoutgroup.labels<-c("Chlamydomonas_reinhardtii","Oryza_sativa","Arabidopsis_thaliana","Populus_trichocarpa")
+      outgroup.labels<-c("Chlamydomonas_reinhardtii","Oryza_sativa","Arabidopsis_thaliana","Populus_trichocarpa")
       }
       else if (input$FUNGIphyloSelect == "Fungi, 5,284 species - Varga et al. 2019") {
-      FUNGIoutgroup.labels<-c("Tremella_polyporina", "Syzygospora_effibulata", "Cryptococcus_podzolicus", "Asterotremella_humicola", "Trichosporon_scarabaeorum", "Trichosporon_domesticum", "Trichosporon_brassicae", "Trichosporon_montevideense", "Trichosporon_mycotoxinivorans", "Trichosporon_loubieri", "Trichosporon_multisporum", "Trichosporon_veenhuisii", "Trichosporon_laibachii", "Trichosporon_gracile", "Trichosporon_dulcitum", "Trichosporon_vadense", "Cryptococcus_humicola", "Trichosporon_wieringae", "Trichosporon_gamsii", "Trichosporon_sporotrichoides", "Trichosporon_porosum", "Trichosporon_inkin", "Trichosporon_coremiiforme", "Trichosporon_faecale", "Trichosporon_japonicum", "Trichosporon_jirovecii", "Trichosporon_debeurmannianum", "Trichosporon_dermatis", "Trichosporon_moniliiforme", "Tremella_globispora", "Cryptococcus_bacillisporus", "Cryptococcus_gattii", "Cryptococcus_neoformans", "Cryptococcus_neoformansvargrubii", "Tsuchiyaea_wingfieldii", "Cryptococcus_amylolentus", "Filobasidiella_depauperata", "Cryptococcus_dejecticola", "Cryptococcus_bestiolae", "Bullera_dendrophila", "Cryptococcus_heveanensis", "Derxomyces_linzhiensis", "Derxomyces_anomala", "Derxomyces_simaoensis", "Derxomyces_komagatae", "Derxomyces_schimicola", "Derxomyces_pseudoschimicola", "Dioszegia_takashimae", "Derxomyces_pseudocylindrica", "Derxomyces_wuzhishanensis", "Derxomyces_hainanensis", "Derxomyces_mrakii", "Derxomyces_boninensis", "Derxomyces_qinlingensis", "Derxomyces_pseudohuiaensis", "Derxomyces_cylindrica", "Derxomyces_waltii", "Derxomyces_nakasei", "Derxomyces_boekhoutii", "Derxomyces_huiaensis", "Derxomyces_hubeiensis", "Derxomyces_yunnanensis", "Sirobasidium_magnum", "Tremella_parmeliarum", "Trimorphomyces_nebularis", "Tremella_fuciformis", "Tremella_brasiliensis", "Tremella_indecorata", "Cryptococcus_laurentii", "Cryptococcus_rajasthanensis", "Cryptococcus_cellulolyticus", "Sirobasidium_brefeldianum", "Tremella_caloplacae", "Tremella_dendrographae", "Tremella_candelariellae", "Tremella_giraffa", "Tremella_cetrariicola", "Biatoropsis_usnearum", "Tremella_tuckerae", "Tremella_coppinsii", "Tremella_wirthii", "Tremella_pertusariae", "Tremella_ramalinae", "Tremella_lichenicola", "Tremella_everniae", "Tremella_hypogymniae", "Tremella_encephala", "Tremella_phaeophysciae", "Tremella_leptogii", "Tremella_lobariacearum", "Tremella_nephromatis", "Tremella_cladoniae", "Tremella_haematommatis", "Tremella_foliacea", "Tremella_mesenterica", "Fibulobasidium_inconspicuum", "Cryptococcus_magnus", "Syzygospora_pallida", "Syzygospora_alba", "Syzygospora_physciacearum", "Syzygospora_bachmannii", "Cryptococcus_terreus", "Cryptococcus_terricola", "Cryptococcus_aerius", "Cryptococcus_saitoi")
+      outgroup.labels<-c("Tremella_polyporina", "Syzygospora_effibulata", "Cryptococcus_podzolicus", "Asterotremella_humicola", "Trichosporon_scarabaeorum", "Trichosporon_domesticum", "Trichosporon_brassicae", "Trichosporon_montevideense", "Trichosporon_mycotoxinivorans", "Trichosporon_loubieri", "Trichosporon_multisporum", "Trichosporon_veenhuisii", "Trichosporon_laibachii", "Trichosporon_gracile", "Trichosporon_dulcitum", "Trichosporon_vadense", "Cryptococcus_humicola", "Trichosporon_wieringae", "Trichosporon_gamsii", "Trichosporon_sporotrichoides", "Trichosporon_porosum", "Trichosporon_inkin", "Trichosporon_coremiiforme", "Trichosporon_faecale", "Trichosporon_japonicum", "Trichosporon_jirovecii", "Trichosporon_debeurmannianum", "Trichosporon_dermatis", "Trichosporon_moniliiforme", "Tremella_globispora", "Cryptococcus_bacillisporus", "Cryptococcus_gattii", "Cryptococcus_neoformans", "Cryptococcus_neoformansvargrubii", "Tsuchiyaea_wingfieldii", "Cryptococcus_amylolentus", "Filobasidiella_depauperata", "Cryptococcus_dejecticola", "Cryptococcus_bestiolae", "Bullera_dendrophila", "Cryptococcus_heveanensis", "Derxomyces_linzhiensis", "Derxomyces_anomala", "Derxomyces_simaoensis", "Derxomyces_komagatae", "Derxomyces_schimicola", "Derxomyces_pseudoschimicola", "Dioszegia_takashimae", "Derxomyces_pseudocylindrica", "Derxomyces_wuzhishanensis", "Derxomyces_hainanensis", "Derxomyces_mrakii", "Derxomyces_boninensis", "Derxomyces_qinlingensis", "Derxomyces_pseudohuiaensis", "Derxomyces_cylindrica", "Derxomyces_waltii", "Derxomyces_nakasei", "Derxomyces_boekhoutii", "Derxomyces_huiaensis", "Derxomyces_hubeiensis", "Derxomyces_yunnanensis", "Sirobasidium_magnum", "Tremella_parmeliarum", "Trimorphomyces_nebularis", "Tremella_fuciformis", "Tremella_brasiliensis", "Tremella_indecorata", "Cryptococcus_laurentii", "Cryptococcus_rajasthanensis", "Cryptococcus_cellulolyticus", "Sirobasidium_brefeldianum", "Tremella_caloplacae", "Tremella_dendrographae", "Tremella_candelariellae", "Tremella_giraffa", "Tremella_cetrariicola", "Biatoropsis_usnearum", "Tremella_tuckerae", "Tremella_coppinsii", "Tremella_wirthii", "Tremella_pertusariae", "Tremella_ramalinae", "Tremella_lichenicola", "Tremella_everniae", "Tremella_hypogymniae", "Tremella_encephala", "Tremella_phaeophysciae", "Tremella_leptogii", "Tremella_lobariacearum", "Tremella_nephromatis", "Tremella_cladoniae", "Tremella_haematommatis", "Tremella_foliacea", "Tremella_mesenterica", "Fibulobasidium_inconspicuum", "Cryptococcus_magnus", "Syzygospora_pallida", "Syzygospora_alba", "Syzygospora_physciacearum", "Syzygospora_bachmannii", "Cryptococcus_terreus", "Cryptococcus_terricola", "Cryptococcus_aerius", "Cryptococcus_saitoi")
       }
       else {
-      FUNGIoutgroup.labels<-c("Neurospora_crassa","Microsporum_canis","Uncinocarpus_reesii","Trichophyton_rubrum","Basipetospora_chlamydospora","Coccidioides_posadasii","Paracoccidioides_brasiliensis","Trichoderma_reesei","Coccidioides_immitis","Histoplasma_capsulatum","Talaromyces_occitanis","Talaromyces_marneffei")
+      outgroup.labels<-c("Neurospora_crassa","Microsporum_canis","Uncinocarpus_reesii","Trichophyton_rubrum","Basipetospora_chlamydospora","Coccidioides_posadasii","Paracoccidioides_brasiliensis","Trichoderma_reesei","Coccidioides_immitis","Histoplasma_capsulatum","Talaromyces_occitanis","Talaromyces_marneffei")
       }
     })
 
@@ -99,22 +99,22 @@ shinyServer(function(input, output, session) {
     })
 
   ## display table of taxa names
-  FUNGItipNames <- eventReactive(input$FUNGIgo, {
-      a<-data.frame(FUNGItree()$tip.label)
+  tipNames <- eventReactive(input$FUNGIgo, {
+      a<-data.frame(tree()$tip.label)
       # replace column name to "full list of taxa"
       colnames(a)[1]<-"full list of taxa for possible subtree"
       # return to df_subset
       return(a)
     })
-  output$FUNGItaxaTable <- renderTable(FUNGItipNames())
+  output$FUNGItaxaTable <- renderTable(tipNames())
   
   ## function to create plot
   output$FUNGIphyloPlot <- renderPlot({
     # root tree
-    tree<-root(FUNGItree(), outgroup = FUNGIoutgroup.labels(), resolve.root = TRUE) 
+    tree<-root(tree(), outgroup = outgroup.labels(), resolve.root = TRUE) 
     # drop outgroup tips not in ingroup.labels
     ingroup.labels<-as.vector(FUNGIdata()$V1)
-    outgroup.labels<-setdiff(FUNGIoutgroup.labels(),ingroup.labels)
+    outgroup.labels<-setdiff(outgroup.labels(),ingroup.labels)
     tree<-drop.tip(tree, outgroup.labels)
     # prune taxa not of interest
     pruned.tree<-try(drop.tip(tree,tree$tip.label[-match(ingroup.labels, tree$tip.label)]), silent=TRUE)
@@ -135,10 +135,10 @@ shinyServer(function(input, output, session) {
       # open the pdf
       pdf(file)
       # root tree
-      tree<-root(tree(), outgroup = FUNGIoutgroup.labels(), resolve.root = TRUE)
+      tree<-root(tree(), outgroup = outgroup.labels(), resolve.root = TRUE)
       # drop outgroup tips not in ingroup.labels
-      ingroup.labels<-as.vector(FUNGIdata()$V1)
-      outgroup.labels<-setdiff(FUNGIoutgroup.labels(),ingroup.labels)
+      ingroup.labels<-as.vector(data()$V1)
+      outgroup.labels<-setdiff(outgroup.labels(),ingroup.labels)
       tree<-drop.tip(tree, outgroup.labels)
       # prune taxa not of interest
       pruned.tree<-try(drop.tip(tree,tree$tip.label[-match(ingroup.labels, tree$tip.label)]), silent=TRUE)
@@ -159,10 +159,10 @@ shinyServer(function(input, output, session) {
       paste("treehouse-",Sys.Date(),".tre",sep= "")},
     content=function(file){
       # root tree
-      tree<-root(FUNGItree(), outgroup = FUNGIoutgroup.labels(), resolve.root = TRUE)
+      tree<-root(tree(), outgroup = outgroup.labels(), resolve.root = TRUE)
       # drop outgroup tips not in ingroup.labels
       ingroup.labels<-as.vector(FUNGIdata()$V1)
-      outgroup.labels<-setdiff(FUNGIoutgroup.labels(),ingroup.labels)
+      outgroup.labels<-setdiff(outgroup.labels(),ingroup.labels)
       tree<-drop.tip(tree, outgroup.labels)
       # prune taxa not of interest
       pruned.tree<-try(drop.tip(tree,tree$tip.label[-match(ingroup.labels, tree$tip.label)]), silent=TRUE)
