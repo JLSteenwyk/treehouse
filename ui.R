79206ddbc3295ad0cbea9ada64a73bb2c81a17f5
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2019-03-24 11:39:43 -0500

    added Jarvis 2016

--- ui.R
+++ ui.R
@@ -102,6 +102,7 @@ shinyUI(fluidPage(
           # drop down select menu to select phylogeny
           selectInput("ANIMALphyloSelect", "Phylogeny:",
             choices=c("Birds, 198 taxa - Prum et al. 2015",
+                      "Birds, 48 taxa - Jarvis et al. 2014",
                       "Metazoans, 36 taxa - Borowiec et al. 2015",
                       "Metazoans, 70 taxa - Whelan et al. 2015",
                       "Vertebrates, 58 taxa - Chen et al. 2015")
